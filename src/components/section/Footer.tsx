ce7c84c00ee4b29bec59f5fbeaf82850c0f5237d
Shivam Singh <sjtsexperience@gmail.com>
2023-10-25 22:51:49 +0530

    fix-footer-grid

--- src/components/section/Footer.tsx
+++ src/components/section/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 justify-items-center pb-10 px-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center pb-10 px-5">
                 <div className='max-w-xs'>
                     <FooterTitle>Address</FooterTitle>
                     <div className='flex gap-3'>
